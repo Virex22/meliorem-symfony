da26b553013275347d6ffa17e5b1a01bf1bd6170
Vincent REMY <vincent@why.vision>
2022-07-22 11:22:59 +0200

    feat : UserController and UserDTO

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\User;
+use App\Service\UserService;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -40,19 +41,21 @@ class AppFixtures extends Fixture
     private ObjectManager $manager;
     private Generator $faker;
     private UserPasswordHasherInterface $hasher;
+    private UserService $userService;
 
-    public function __construct(UserPasswordHasherInterface $hasher)
+    public function __construct(UserPasswordHasherInterface $hasher,UserService $userService)
     {
         $this->hasher = $hasher;
         $this->faker = Factory::create('fr_FR');
+        $this->userService = $userService;
     }
 
     public function load(ObjectManager $manager): void
     {
         $this->manager = $manager;
 
-        $this->createUser(["ROLE_STUDENT"],5,"student");
-        $this->createUser(["ROLE_SPEAKER"],5,"speaker");
+        $this->createStudent(5);
+        $this->createSpeaker(5);
         $this->createUser(["ROLE_ADMINISTRATION"],5,"administration");
         $this->createUser(["ROLE_SUPERADMIN"],5,"superadmin");
 
@@ -69,13 +72,46 @@ class AppFixtures extends Fixture
             ->setName($this->faker->name())
             ->setImage("https://picsum.photos/1000/700")
             ->setPassword($this->hasher->hashPassword($user, 'azerty'));
+
             $returnUsers[] = $user;
             $this->manager->persist($user);
         }
         return $returnUsers;
     }
 
+    public function createStudent($count){
+        $returnUsers = [];
+        for ($i=0; $i < $count; $i++) { 
+            $user = new User();
+            $user->setEmail("student$i@meliorem.fr")
+            ->setRoles(["ROLE_STUDENT"])
+            ->setFirstname($this->faker->firstName())
+            ->setName($this->faker->name())
+            ->setImage("https://picsum.photos/1000/700")
+            ->setPassword($this->hasher->hashPassword($user, 'azerty'));
 
-    
-    
+            $this->userService->createStudentPair($user);
+            $returnUsers[] = $user;
+            $this->manager->persist($user);
+        }
+        return $returnUsers;
+    }
+
+    public function createSpeaker($count){
+        $returnUsers = [];
+        for ($i=0; $i < $count; $i++) { 
+            $user = new User();
+            $user->setEmail("speaker$i@meliorem.fr")
+            ->setRoles(["ROLE_SPEAKER"])
+            ->setFirstname($this->faker->firstName())
+            ->setName($this->faker->name())
+            ->setImage("https://picsum.photos/1000/700")
+            ->setPassword($this->hasher->hashPassword($user, 'azerty'));
+
+            $this->userService->createSpeakerPair($user);
+            $returnUsers[] = $user;
+            $this->manager->persist($user);
+        }
+        return $returnUsers;
+    }
 }
